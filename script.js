b8beb046c73d3019e1129dd26fb9707aaa1a15a5
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-01-31 13:46:59 +0900

    jqueryadd

---
+++ script.js
@@ -0,0 +1,13 @@
+$ (function () {
+    var count = $('.time').text().length;
+    $('.time').css('font-size', 150/count + 'vw');
+});
+
+$(document).ready(function(){
+    $('.datepicker').datepicker({
+        format:"yyyy/mm/dd"
+    });
+    $('.timepicker').timepicker(
+        {twelveHour:"false"}
+    );
+  });
