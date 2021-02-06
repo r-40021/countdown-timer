bcd825413c432c6144c532dbd141cd36901940c4
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-02-06 20:13:27 +0900

    時刻初期表示追加

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 $ (function () {
     var count = $('.time').text().length;
-    $('.time').css('font-size', 150/count + 'vw');
+    $('.time').css('font-size', 100/count + 'vw');
+    var date = new Date();
+    var date2 = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate()
+    var minute = date.getMinutes()
+    if (minute < 10) {
+        minute = "0" + minute
+    }
+    var SetTime = date.getHours() + ":" + minute
+    $("#Date").val(date2);
+    $("#Time").val(SetTime);
 });
+
+function set() {
+    var myquery = $(location).attr('search');
+    console.log(myquery);
+}
 
 $(document).ready(function(){
     $('.datepicker').datepicker({
-        format:"yyyy/mm/dd"
+        format:"yyyy/mm/dd",
+        autoClose:true
     });
-    $('.timepicker').timepicker(
-        {twelveHour:"false"}
-    );
+    $('.timepicker').timepicker({
+        twelveHour:false,
+        autoClose:true
+    });
+    $('.fixed-action-btn').floatingActionButton();
   });
