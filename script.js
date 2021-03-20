1b8af2f6a11cc73a71bf0d5d82457182ea64397f
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-03-20 11:37:20 +0900

    player をinlineに

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function onload() {
         
     } else{
         document.getElementById("audioInput").style.display = "inline";
-        document.getElementById("player").style.display = "block";
+        document.getElementById("player").style.display = "inline";
     }
     resize();　//文字サイズ調整
     /*パラメータ取得*/
