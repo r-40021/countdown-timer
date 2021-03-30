c1163066a96ef9115f9b716c291c616fc61c8910
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-03-30 12:59:02 +0900

    脱jquery

--- sw.js
+++ sw.js
@@ -13,67 +13,70 @@ const urlsToCache = [
   'push.min.js'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
-    // キャッシュを開く
     caches.open(CACHE_NAME)
-    .then((cache) => {
-      // 指定されたファイルをキャッシュに追加する
-      return cache.addAll(urlsToCache);
-    })
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['20210330v3'];
+
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return cacheNames.filter((cacheName) => {
-        // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
-        return cacheName.startsWith(`${registration.scope}!`) &&
-               cacheName !== CACHE_NAME;
-      });
-    }).then((cachesToDelete) => {
-      return Promise.all(cachesToDelete.map((cacheName) => {
-        // いらないキャッシュを削除する
-        return caches.delete(cacheName);
-      }));
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-    .then((response) => {
-      // キャッシュ内に該当レスポンスがあれば、それを返す
-      if (response) {
-        return response;
-      }
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-      // 重要：リクエストを clone する。リクエストは Stream なので
-      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-      // 必要なので、リクエストは clone しないといけない
-      let fetchRequest = event.request.clone();
+        // IMPORTANT:Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
 
-      return fetch(fetchRequest)
-        .then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            // キャッシュする必要のないタイプのレスポンスならそのまま返す
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT:Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
             return response;
           }
-
-          // 重要：レスポンスを clone する。レスポンスは Stream で
-          // ブラウザ用とキャッシュ用の2回必要。なので clone して
-          // 2つの Stream があるようにする
-          let responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        });
-    })
-  );
+        );
+      })
+    );
 });
