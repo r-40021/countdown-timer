8d9c849794460acff7e0e54ebabb06e9238d419f
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-01 12:15:06 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheAllowlist = ['20210330v4','20210330v3'];
+  var cacheAllowlist = ['20210401','20210330v4','20210330v3'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
