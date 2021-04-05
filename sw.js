06578dade8dbabfd05407c1c20511fa483a5b8ed
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-05 19:04:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '20210405v6';
+const CACHE_NAME = '20210405v7';
 
 // キャッシュするファイルをセットする
 const urlsToCache = [
@@ -27,7 +27,7 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate', function (event) {
 
-  var cacheAllowlist = ['20210405v6'];
+  var cacheAllowlist = ['20210405v7'];
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
