b7288377b6ff62c76931c47afffc1aa4ced972a1
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-04-10 16:54:07 +0900

    レイアウト調整

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '20210410v6';
+const CACHE_NAME = '20210410v7';
 const urlsToCache = [
   'index.html',
   'style.css',
@@ -25,7 +25,7 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate', function (event) {
 
-  var cacheAllowlist = ['20210410v6'];
+  var cacheAllowlist = ['20210410v7'];
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
