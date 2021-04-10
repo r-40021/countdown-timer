65c40c33a24135b75d1caa91888bace0cd6b6eb9
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-10 13:16:36 +0900

    バグ修正(iPhone)

--- script.js
+++ script.js
@@ -437,6 +437,8 @@ function flex() {
   /*スマホのURLバーに隠されないように*/
   var height = window.innerHeight;
   document.getElementsByClassName('flex')[0].style.height = height + 'px';
+　document.html.style.height = height + 'px';
+  document.body.style.height = height + 'px';
 }
 window.addEventListener('resize', function () {
   resize();
