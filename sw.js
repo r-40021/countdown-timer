8b4b7a9eecc7d11ca470808f29e0773ed72a48d7
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-16 20:50:17 +0900

    Add privacy policy button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '20210411v3';
+const CACHE_NAME = '20210416';
 const urlsToCache = [
   'index.html',
   'style.css',
@@ -25,7 +25,7 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate', function (event) {
 
-  var cacheAllowlist = ['20210411v3'];
+  var cacheAllowlist = ['20210416'];
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
