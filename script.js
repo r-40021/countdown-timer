3c2bf99dfde713756450b3c332ccdb952e10011f
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-04-18 16:01:52 +0900

    バグ修正

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var noSleep = new NoSleep();
 /*Service Worker*/
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
-    navigator.serviceWorker.register("sw.js").then(
+    navigator.serviceWorker.register("/countdown-timer/sw.js").then(
       function (registration) {
         // Registration was successful
         console.log(
