5da07fe99fc8d7e723446415db0009fd5f21978b
Kenichi Yamada <75155258+r-40021@users.noreply.github.com>
2021-05-08 17:58:42 +0900

    バグ修正

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'ct-20210504';
+var CACHE_NAME = 'ct-20210508';
 var urlsToCache = [
   '/countdown-timer/index.html',
   '/countdown-timer/style.css',
@@ -12,71 +12,27 @@ var urlsToCache = [
   '/countdown-timer/push.min.js.map',
   '/countdown-timer/nosleep.min.js'
 ];
-
-self.addEventListener('install', function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', function (event) {
-
-  var cacheAllowlist = ['20210418'];
-
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT:Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
+self.addEventListener("install", (function(event) {
+    event.waitUntil(caches.open(CACHE_NAME).then((function(cache) {
+        return console.log("Opened cache"), cache.addAll(urlsToCache)
+    })))
+})), self.addEventListener("activate", (function(event) {
+    var cacheAllowlist = [CACHE_NAME];
+    event.waitUntil(caches.keys().then((function(cacheNames) {
+        return Promise.all(cacheNames.map((function(cacheName) {
+            if (-1 === cacheAllowlist.indexOf(cacheName)) return caches.delete(cacheName)
+        })))
+    })))
+})), self.addEventListener("fetch", (function(event) {
+    event.respondWith(caches.match(event.request).then((function(response) {
+        if (response) return response;
         var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function (response) {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT:Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
+        return fetch(fetchRequest).then((function(response) {
+            if (!response || 200 !== response.status || "basic" !== response.type) return response;
             var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function (cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-  );
-});
+            return caches.open(CACHE_NAME).then((function(cache) {
+                cache.put(event.request, responseToCache)
+            })), response
+        }))
+    })))
+}));
