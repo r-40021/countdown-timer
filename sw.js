d3efc5fd7fe1532712e827da821fdf031ff3400e
Frog <75155258+r-40021@users.noreply.github.com>
2021-10-30 12:27:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,9 @@ self.addEventListener("install", (function(event) {
     event.waitUntil(caches.open(CACHE_NAME).then((function(cache) {
         return console.log("Opened cache"), cache.addAll(urlsToCache)
     })))
+  event.waitUntil(self.skipWaiting());
 })), self.addEventListener("activate", (function(event) {
+  event.waitUntil(self.clients.claim()); //即座に更新
     var cacheAllowlist = [CACHE_NAME];
     event.waitUntil(caches.keys().then((function(cacheNames) {
         return Promise.all(cacheNames.map((function(cacheName) {
