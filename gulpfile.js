69f39effd0faf4a556bc67de97c822e8e6e306c7
Frog <75155258+r-40021@users.noreply.github.com>
2021-12-27 20:42:55 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ const compileSass = () =>
 /**
  * Sassファイルを監視し、変更があったらSassを変換します
  */
-const watchSassFiles = () => watch("./src/style.scss", compileSass);
+// const watchSassFiles = () => watch("./src/style.scss", compileSass);
 
 // npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
-exports.default = watchSassFiles;
+exports.default = compileSass;
