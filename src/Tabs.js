cad717b85f6f84a8b977f21fa45b1692df4c0026
Frog <frog@frogapp.net>
2022-10-10 19:15:33 +0900

    Delete files

--- src/Tabs.js
+++
@@ -1,9 +0,0 @@
-let timeoutLet;
-export function recalTabs() {
-  clearTimeout(timeoutLet);
-  document.getElementById("settings").style.transform = "scale(1)";
-  timeoutLet = setTimeout(() => {
-    M.Tabs.getInstance(document.getElementById("settingsTab")).updateTabIndicator();
-    document.getElementById("settings").style.transform = "";
-  }, 600);
-}
