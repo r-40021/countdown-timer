c3b0a582165aabe3fc972dac1f5a700730f0c1fd
FrogApp <frog@frogapp.net>
2023-07-15 17:23:33 +0900

    Add server

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,9 @@ module.exports = {
   // ローカル開発用環境を立ち上げる
   // 実行時にブラウザが自動的に localhost を開く
   devServer: {
-    contentBase: "./",
+    static: {
+      directory: './'
+    },
     open: true
   },
   plugins: [
